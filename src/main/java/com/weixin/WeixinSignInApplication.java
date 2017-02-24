b91d55feed4c7125e5f2ea302d94fb0d3ce4188f
gtli <gtli@gaoxiaobang.com>
2017-02-24 21:26:34 +0800

    init project

---
+++ src/main/java/com/weixin/WeixinSignInApplication.java
@@ -0,0 +1,12 @@
+package com.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinSignInApplication.class, args);
+	}
+}
